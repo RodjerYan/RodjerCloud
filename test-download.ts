2f11c9be4b6a414f6ab68fb7cb764a62f26cb593
Alexander <alexander@MacBook-Air-Alexander.local>
2026-07-22 23:12:10 +0300

    chore: remove test-download.ts

--- test-download.ts
+++
@@ -1,2 +0,0 @@
-import { Api } from 'telegram/tl'
-console.log(Object.keys(new Api.upload.GetFile({ location: new Api.InputDocumentFileLocation({ id: BigInt(1), accessHash: BigInt(1), fileReference: Buffer.from([]), thumbSize: '' }), offset: BigInt(0), limit: 1024 })))
